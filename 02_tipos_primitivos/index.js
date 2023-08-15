2aacb59b9f8ff97b115bfbd6b33b21282f717d1a
Maria beatriz Lacerda de oliveira <beatrizlacerda2011@gmail.com>
2023-08-15 17:41:41 -0300

    tipos_primitivo-if-for

---
+++ 02_tipos_primitivos/index.js
@@ -0,0 +1,15 @@
+var idade = [1,2,3]
+
+console.log(
+    typeof(idade)
+)
+
+var usuario = {
+    nome : 'Maria',
+    idade : 19,
+    sexo : 'feminino'
+}
+
+console.log( usuario ) 
+
+// função typeof() serve para mostrar o tipo da variável
---
+++ 02_tipos_primitivos/index2.js
@@ -0,0 +1,26 @@
+var num1 = 300
+var num2 = 1324
+
+// if (num1+num2 > 1500){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
+// for(let i = 0; i <= 10; i++){
+//     num1 = num1 + i
+//     console.log(num1)
+// }
+
+var adm = true
+var erro = null
+
+if(!erro){
+    console.log('houve um erro return')
+}
+
+if(adm){
+    console.log('you are adm')
+}
+
